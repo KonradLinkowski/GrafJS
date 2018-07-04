662f667673550510cd17b43a78354783eb6c7e0a
Konrad Linkowski <linkowski.konrad97@gmail.com>
2018-07-05 00:16:25 +0200

    Added chunk get and add

--- graph.js
+++ graph.js
@@ -1,7 +1,28 @@
-const node = require('./node.js')
+const Node = require('./node.js')
 
 export default class Graph {
-  constructor() {
+  constructor(defaultValue) {
     this.chunks = []
+    this.defaultValue = defaultValue
+  }
+  addChunk(x, y, value) {
+    const chunk = new Node(x, y, value || this.defaultValue)
+    const prefs = {
+      up: this.getChunk(x, y - 1),
+      down: this.getChunk(x, y + 1),
+      left: this.getChunk(x - 1, y),
+      right: this.getChunk(x + 1, y)
+    }
+    chunk.sides = prefs
+    prefs.up.sides.down = chunk
+    prefs.down.sides.up = chunk
+    prefs.left.sides.right = chunk
+    prefs.right.sides.left = chunk
+    this.chunks.push(chunk)
+  }
+  getChunk(x, y) {
+    return this.chunks.find(c => {
+      return c.x === x && c.y == y
+    }) || null
   }
 }
